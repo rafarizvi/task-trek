e56bb292d4f30054ee6e83e354a29e3c7c8d606f
danielhe27 <jdah49d@gmail.com>
2024-03-25 21:52:19 -0400

    index

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const users = require('./user'); 
 const task = require('./task');
-
+const profile = require('./profile');
 
 User.hasOne(Profile, {
   foreignKey: 'userId', // Asegúrate de definir esta clave foránea en tu modelo Profile
@@ -12,9 +12,16 @@ Profile.belongsTo(User, {
   foreignKey: 'userId'
 });
 
+User.hasMany(Task, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE',
+});
+Task.belongsTo(User, {
+  foreignKey: 'userId',
+});
 
 
 
 
 
-module .exports = { users, tasks};
+module .exports = { users, Profile, tasks };
