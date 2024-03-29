22d5c66241d05aed0115e4a5646e90a16341ab42
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-03-28 20:06:04 -0400

    updated index file in model folder

--- models/index.js
+++ models/index.js
@@ -2,10 +2,6 @@
 const User = require('./User'); 
 const Profile = require('./Profile');
 const Task = require('./Task');
-const PendingTask = require('./PendingTask');
-const InprogressTask = require('./InprogressTask');
-const CompletedTask = require('./CompletedTask');
-
 
 User.hasOne(Profile, {
   foreignKey: 'userId', // Asegúrate de definir esta clave foránea en tu modelo Profile
@@ -26,32 +22,4 @@ Task.belongsTo(User, {
   foreignKey: 'userId',
 });
 
-User.hasMany(PendingTask, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE',
-});
-
-PendingTask.belongsTo(User, {
-  foreignKey: 'userId',
-});
-
-User.hasMany(InprogressTask, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE',
-});
-
-InprogressTask.belongsTo(User, {
-  foreignKey: 'userId',
-});
-
-User.hasMany(CompletedTask, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE',
-});
-
-CompletedTask.belongsTo(User, {
-  foreignKey: 'userId',
-});
-
-
-module .exports = { User, Profile, Task, PendingTask, InprogressTask, CompletedTask };
+module .exports = { User, Profile, Task };
