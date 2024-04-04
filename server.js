81e3f85e7037e3568d5d90ddee2391aa3c7b27d6
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-04-04 18:50:35 -0400

    uncommentted code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+require('dotenv').config();
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
