ce7389bf44389bc6459ba34dc1fb70812af30a6b
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-04-07 18:08:36 -0400

    handeled err when registering using duplicate email

--- controllers/api/registerRoute.js
+++ controllers/api/registerRoute.js
@@ -2,14 +2,21 @@ const { User } = require('../../models');
 
 exports.registerUser = async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+        const checkUserData = await User.findOne({ where: { email: req.body.email } });
 
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+        if (!checkUserData) {
+            const userData = await User.create(req.body);
 
-            res.redirect('/');
-        });
+            req.session.save(() => {
+                req.session.user_id = userData.id;
+                req.session.logged_in = true;
+                res.redirect('/');
+            });
+        }
+        else {
+            
+            res.status(401).send('An account is already registered using this email');
+        }
     } catch (err) {
         res.status(400).json(err);
     }
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,21 +7,6 @@ const registerRoute = require('./registerRoute');
 
 router.post('/register', registerRoute.registerUser);
 
-
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      res.redirect('/');
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 router.post('/login', async (req, res) => {
   try {
     console.log(req.body);
